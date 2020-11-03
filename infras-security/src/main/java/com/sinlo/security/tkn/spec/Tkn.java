6397bc4fc4cd391d7273e7ca3bfe0ad969df3131
liuxy <391861737@qq.com>
2020-11-03 20:27:43 +0800

    security: Add some useful factory methods for Tkn

--- infras-security/src/main/java/com/sinlo/security/tkn/spec/Tkn.java
+++ infras-security/src/main/java/com/sinlo/security/tkn/spec/Tkn.java
@@ -25,8 +25,41 @@ public class Tkn<T> {
         this.longevous = longevous;
     }
 
+    /**
+     * Create a {@link Tkn} without any check
+     */
     public static <T> Tkn<T> of(T ephemeral, T longevous) {
         return new Tkn<>(ephemeral, longevous);
+    }
+
+    /**
+     * Create a {@link Tkn} with only a {@link #ephemeral} token, it will throw a
+     * {@link TknException.Null} if the given ephemeral token is null
+     */
+    public static <T> Tkn<T> ephemeral(T ephemeral) {
+        if (ephemeral == null)
+            throw new TknException.Null();
+        return of(ephemeral, null);
+    }
+
+    /**
+     * Create a {@link Tkn} with only a {@link #longevous} token, it will throw a
+     * {@link TknException.Null} if the given longevous token is null
+     */
+    public static <T> Tkn<T> longevous(T longevous) {
+        if (longevous == null)
+            throw new TknException.Null();
+        return of(null, longevous);
+    }
+
+    /**
+     * Create a {@link Tkn} with both {@link #ephemeral} and {@link #longevous} tokens, it will
+     * throw a {@link TknException.Null} if any one of the given tokens is null
+     */
+    public static <T> Tkn<T> both(T ephemeral, T longevous) {
+        if (ephemeral == null || longevous == null)
+            throw new TknException.Null();
+        return of(ephemeral, longevous);
     }
 
     /**
