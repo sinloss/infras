d3a0bb81856414943df8654d111ad9d1ea91a80f
liuxy <391861737@qq.com>
2020-12-07 12:50:52 +0800

    Print more detailed information for every unexpected exceptions

--- infras-sponte/src/main/java/com/sinlo/sponte/core/Spontaneously.java
+++ infras-sponte/src/main/java/com/sinlo/sponte/core/Spontaneously.java
@@ -7,10 +7,13 @@ import javax.annotation.processing.*;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.AnnotationMirror;
 import javax.lang.model.element.TypeElement;
+import javax.tools.Diagnostic;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.BiConsumer;
+import java.util.stream.Collectors;
 
 import static com.sinlo.sponte.Sponte.*;
 
@@ -37,34 +40,42 @@ public class Spontaneously extends AbstractProcessor {
     @Override
     public boolean process(Set<? extends TypeElement> types,
                            RoundEnvironment roundEnv) {
-        if (round++ != 0) return true;
-        // get the current stage
-        Stage stage = Stage.which();
-        // get all existed sponted annotation names
-        Set<String> existed = Fo.SPONTED.names();
+        try {
+            if (round++ != 0) return true;
+            // get the current stage
+            Stage stage = Stage.which();
+            // get all existed sponted annotation names
+            Set<String> existed = Fo.SPONTED.names();
 
-        final AtomicBoolean erred = new AtomicBoolean(false);
-        // the proc in which the main process logic is being held
-        BiConsumer<TypeElement, TypeElement> proc = (t, a) -> {
-            Context ctx = new Context(this.processingEnv, t, existed);
-            roundEnv.getElementsAnnotatedWith(a).stream()
-                    .map(ctx::subject).forEach(stage::process);
-            erred.compareAndSet(false, !ctx.close());
-        };
+            final AtomicBoolean erred = new AtomicBoolean(false);
+            // the proc in which the main process logic is being held
+            BiConsumer<TypeElement, TypeElement> proc = (t, a) -> {
+                Context ctx = new Context(this.processingEnv, t, existed);
+                roundEnv.getElementsAnnotatedWith(a).stream()
+                        .map(ctx::subject).forEach(stage::process);
+                erred.compareAndSet(false, !ctx.close());
+            };
 
-        if (Stage.INHERIT.equals(stage)) {
-            // iterate all the annotation presented in the existed set,
-            // which is the annotation that are inherited by the current
-            // processing elements
-            types.forEach(a -> a.getAnnotationMirrors().stream()
-                    .map(AnnotationMirror::getAnnotationType)
-                    .filter(t -> existed.contains(t.toString()))
-                    .map(t -> (TypeElement) t.asElement())
-                    .forEach(t -> proc.accept(t, a)));
-        } else {
-            types.forEach(a -> proc.accept(a, a));
+            if (Stage.INHERIT.equals(stage)) {
+                // iterate all the annotation presented in the existed set,
+                // which is the annotation that are inherited by the current
+                // processing elements
+                types.forEach(a -> a.getAnnotationMirrors().stream()
+                        .map(AnnotationMirror::getAnnotationType)
+                        .filter(t -> existed.contains(t.toString()))
+                        .map(t -> (TypeElement) t.asElement())
+                        .forEach(t -> proc.accept(t, a)));
+            } else {
+                types.forEach(a -> proc.accept(a, a));
+            }
+            return !erred.get() && !roundEnv.errorRaised();
+        } catch (Exception e) {
+            processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR,
+                    Arrays.stream(e.getStackTrace())
+                            .map(StackTraceElement::toString)
+                            .collect(Collectors.joining("\n")));
         }
-        return !erred.get() && !roundEnv.errorRaised();
+        return false;
     }
 
     @Override
