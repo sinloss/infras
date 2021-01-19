e89b2cfd061e5bb55b2a7f6135a56e8f8913209c
liuxy <391861737@qq.com>
2021-01-19 19:11:03 +0800

    Collin: Introduce

---
+++ infras-core/src/main/java/com/sinlo/core/common/util/Collin.java
@@ -0,0 +1,20 @@
+package com.sinlo.core.common.util;
+
+import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+/**
+ * Collin the collector of {@link java.util.stream.Collector}s
+ *
+ * @author sinlo
+ */
+public class Collin {
+
+    /**
+     * The collector for {@link ConcurrentSkipListSet}
+     */
+    public static <T> Collector<T, ?, ConcurrentSkipListSet<T>> toSkipList() {
+        return Collectors.toCollection(ConcurrentSkipListSet::new);
+    }
+}
